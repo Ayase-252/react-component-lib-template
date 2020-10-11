bef70b8af8fc788a230ab396bb53e0771feb4998
Qingyu Deng <bitdqy@hotmail.com>
2020-10-11 20:03:58 +0800

    feat: add main build process

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import styles from "./button.module.less";
 
 const Button = () => {
-  return <Button></Button>;
+  return <Button className={styles.button}></Button>;
 };
 
 export default Button;
